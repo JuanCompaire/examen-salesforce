15762f11176d63afc34717ebaa7ba5d81b92f134
JuanCompaire <juancompaire2004@gmail.com>
2024-02-19 09:57:59 +0100

    no se ni que estoy haciendo con apex

--- Examen/force-app/main/default/aura/BotonAction/BotonActionController.js
+++ Examen/force-app/main/default/aura/BotonAction/BotonActionController.js
@@ -4,12 +4,36 @@
     },
     grabar : function(component, event, helper) {
 
+        component.set("v.recording", true);
         component.set("v.lista", []);
 
     },
     stop : function(component, event, helper) {
+        component.set("v.recording", false);
+        var secuencias = component.get("v.lista");
+        var accion = component.get("c.guardarSecuencias");
+        accion.setParams({"secuencia": secuencias});
+        accion.setCallback(this, function(response){
+            var state = response.getState();
+            if (state === "SUCCESS") {
+                component.set("v.lista", []);
+                console.log("Secuencias guardadas");
+            }
+            else{
+                console.log("Error al guardar secuencias");
+            }
+        });
+        $A.enqueueAction(action);
+
+
     },
     play : function(component, event, helper) {
+
+        var secuencias = component.get("v.lista");
+        secuencias.forEach(function(secuencia,index){
+            console.log("Secuencia " + (index + 1) + ": " + secuencia.join(' '));
+
+    });
     },
     clear : function(component, event, helper) {
         component.set("v.lista", []);
@@ -17,9 +41,10 @@
     addToList : function(component, event, helper) {
         var lista = component.get("v.lista");
         var item = event.getParam("number");
-        lista.push(item);
-        component.set("v.lista", lista);
-
-        
+        if (component.get("v.recording")){
+            
+            lista.push(item);
+            component.set("v.lista", lista);
+        }        
     }
 })

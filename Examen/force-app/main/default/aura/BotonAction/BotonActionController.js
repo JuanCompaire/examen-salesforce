36ad5b237420406d3b281049d379a978be0aaf84
JuanCompaire <juancompaire2004@gmail.com>
2024-02-19 10:39:26 +0100

    intentamos el boton save

--- Examen/force-app/main/default/aura/BotonAction/BotonActionController.js
+++ Examen/force-app/main/default/aura/BotonAction/BotonActionController.js
@@ -1,41 +1,44 @@
 ({
-    myAction : function(component, event, helper) {
-
-    },
     grabar : function(component, event, helper) {
-
         component.set("v.recording", true);
         component.set("v.lista", []);
-
+        component.set("v.listaTemporal", []);
     },
     stop : function(component, event, helper) {
         component.set("v.recording", false);
         var lista = component.get("v.lista");
-        var lista1 = lista;
-
-        
-        component.set("v.lista", []);
-
-        console.log(lista1.join(' '));
+        var secuenciaNow = component.get("v.listaTemporal");
+        if( secuenciaNow && secuenciaNow.length > 0){
+            lista.push(secuenciaNow);
+        }
+        component.set("v.listaTemporal", []);
+        component.set("v.lista", lista);
     },
     play : function(component, event, helper) {
-
         var secuencias = component.get("v.lista");
-        secuencias.forEach(function(secuencia,index){
-            console.log("Secuencia " + (index + 1) + ": " + secuencia.join(' '));
 
-    });
+        function mostrarSecuenciaConRetraso(index){
+            if(index < secuencias.length){
+                console.log("Secuencia " + ": " + secuencias[index].join(' '));
+                setTimeout(function() {
+                    mostrarSecuenciaConRetraso(index + 1);
+                }, 1000); 
+            }
+        }
+        mostrarSecuenciaConRetraso(0);
     },
     clear : function(component, event, helper) {
         component.set("v.lista", []);
+        component.set("v.listaTemporal", []);
+    },
+    save : function(component, event, helper) {
     },
     addToList : function(component, event, helper) {
-        var lista = component.get("v.lista");
+        var listaTemporal = component.get("v.listaTemporal");
         var item = event.getParam("number");
         if (component.get("v.recording")){
-            
-            lista.push(item);
-            component.set("v.lista", lista);
+            listaTemporal.push(item);
+            component.set("v.listaTemporal", listaTemporal);
         }        
     }
 })

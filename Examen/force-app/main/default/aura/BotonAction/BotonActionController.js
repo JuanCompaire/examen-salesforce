d0222ad3d1e6793217a9f577d537d806ea096617
JuanCompaire <juancompaire2004@gmail.com>
2024-02-19 10:59:32 +0100

    no va

--- Examen/force-app/main/default/aura/BotonAction/BotonActionController.js
+++ Examen/force-app/main/default/aura/BotonAction/BotonActionController.js
@@ -32,6 +32,23 @@
         component.set("v.listaTemporal", []);
     },
     save : function(component, event, helper) {
+
+        var secuencias = component.get("v.lista");
+
+        var action = component.get("c.saveSecuencias");
+        action.setParams({
+            "secuencias": secuencias
+        });
+        action.setCallback(this, function(response){
+            var state = response.getState();
+            if (state === "SUCCESS") {
+                console.log("Secuencias guardadas");
+            }
+            else {
+                console.log("Error al guardar las secuencias");
+            }
+        });
+        $A.enqueueAction(action);
     },
     addToList : function(component, event, helper) {
         var listaTemporal = component.get("v.listaTemporal");
